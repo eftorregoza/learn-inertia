1b25f960a2b002d9ef308204b644e365d88adea4
Exxel <exxeltorregoza@gmail.com>
2025-08-02 16:53:18 +0800

    Fix Settings page

--- resources/js/Pages/Settings.vue
+++ resources/js/Pages/Settings.vue
@@ -6,22 +6,10 @@ defineOptions({
     layout: Layout
 });
 
-defineProps({
-    time: String
-});
-
 </script>
 
 <template>
     <h1 class="text-3xl">
-        Users
+        Settings
     </h1>
-
-    <div style="margin-top: 400px">
-        <p>The current time is {{ time }}.</p>
-
-        <Link href="/users" class="text-blue-500" preserve-scroll>
-        Refresh
-        </Link>
-    </div>
 </template>
