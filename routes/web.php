de231e21f9da657843059859c9b3e869f39c5e10
Exxel <exxeltorregoza@gmail.com>
2025-08-03 15:40:21 +0800

    Inertia Authorization
    Add login and logout features

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Models\User;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
@@ -7,59 +8,60 @@ use Illuminate\Validation\Rules\Password;
 use Illuminate\Support\Str;
 use Inertia\Inertia;
 
+Route::get('/login', [LoginController::class, 'create'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+Route::post('/logout', [LoginController::class, 'destroy'])->middleware('auth');
 
-Route::get('/', function () {
-    $name = 'John Doe';
+Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        $name = 'John Doe';
 
-    return Inertia::render('Home', [
-        'name' => $name
-    ]);
-});
+        return Inertia::render('Home', [
+            'name' => $name
+        ]);
+    });
 
-Route::get('/users', function () {
-    return Inertia::render('Users/Index', [
-        // 'users' => User::paginate(10)->through(fn($user) => [
-        //     'id' => $user->id,
-        //     'name' => $user->name
-        // ])
-        'users' => User::query()
-            ->when(Request::input('search'), function ($query, $search) {
-                $query->where('name', 'LIKE', "%{$search}%");
-            })
-            ->orderByDesc('created_at')
-            ->paginate(10)
-            ->withQueryString()
-            ->through(fn($user) => [
-                'id' => $user->id,
-                'name' => $user->name
-            ]),
-        'filters' => Request::only(['search'])
-    ]);
-});
+    Route::get('/users', function () {
+        return Inertia::render('Users/Index', [
+            // 'users' => User::paginate(10)->through(fn($user) => [
+            //     'id' => $user->id,
+            //     'name' => $user->name
+            // ])
+            'users' => User::query()
+                ->when(Request::input('search'), function ($query, $search) {
+                    $query->where('name', 'LIKE', "%{$search}%");
+                })
+                ->orderByDesc('created_at')
+                ->paginate(10)
+                ->withQueryString()
+                ->through(fn($user) => [
+                    'id' => $user->id,
+                    'name' => $user->name
+                ]),
+            'filters' => Request::only(['search'])
+        ]);
+    });
 
-Route::get('/users/create', function() {
-    return Inertia::render('Users/Create');
-});
+    Route::get('/users/create', function () {
+        return Inertia::render('Users/Create');
+    });
 
-Route::post('/users/create', function() {
-    $attributes = Request::validate([
-        'name' => ['required'],
-        'email' => ['required', 'email'],
-        'password' => ['required', Password::min(8)],
-    ]);
+    Route::post('/users/create', function () {
+        $attributes = Request::validate([
+            'name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', Password::min(8)],
+        ]);
 
-    $attributes['remember_token'] = Str::random(10);
+        $attributes['remember_token'] = Str::random(10);
 
-    User::create($attributes);
+        User::create($attributes);
 
-    return redirect('/users');
-});
+        return redirect('/users');
+    });
 
-Route::get('/settings', function () {
-    return Inertia::render('Settings');
-});
+    Route::get('/settings', function () {
+        return Inertia::render('Settings');
+    });
 
-Route::post('/logout', function () {
-    dump('Logging out...');
-    dd('Logged out!');
 });
